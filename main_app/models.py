2ff5ff2b2a056bcc50d4bcddb9e2e5de08fe2a25
Jia Wang <daisy-a.119@hotmail.com>
2024-03-04 11:19:30 -0500

    add mc date picker

--- main_app/models.py
+++ main_app/models.py
@@ -1,15 +1,27 @@
 from django.db import models
 from django.urls import reverse
 
+
+ACTIVITY_TYPE = (
+  ('W', 'Walk'),
+  ('R', 'Run'),
+  ('B', 'Bike'),
+  ('R', 'Rollerblade'),
+)
+
 class Activity(models.Model):
-  date = models.DateField()
-  duration = models.DurationField()
+  date = models.DateField('Activity date')
+  duration = models.DurationField('Activity duration')
   calorie_burnt = models.IntegerField()
   distance = models.FloatField()
   city = models.CharField(max_length=20)
   description = models.TextField(max_length=250)
   max_heart_rate = models.IntegerField()
-  activity_type = models.CharField(max_length=20)
+  activity_type = models.CharField(
+    max_length=20,
+    choices=ACTIVITY_TYPE,
+    default=ACTIVITY_TYPE[0][0]
+    )
 
   def __str__(self):
     return self.activity_type   
